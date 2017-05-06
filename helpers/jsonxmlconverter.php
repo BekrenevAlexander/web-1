31173b04aaf3dfbd8a26377d88fb3583fac5bcf4
BekrenevA <bek-aleksander@yandex.ru>
2017-05-06 12:22:16 +0500

    add json xml

---
+++ helpers/jsonxmlconverter.php
@@ -0,0 +1,42 @@
+﻿<?php
+setlocale (LC_CTYPE, "ru_RU.UTF-8");
+require 'classes/Users.php';
+require 'classes/Group.php';
+require 'classes/MembersOfGroup.php';
+
+class pars {
+
+    
+
+    static function ObjecttoJSON($object){
+        return json_encode($object,JSON_UNESCAPED_UNICODE);
+    }
+    
+    static function JSONtoXML($json) {
+        $xml=new SimpleXMLElement('<data/>');
+        pars::arrayToXml($json, $xml);
+        return html_entity_decode($xml->asXML(),ENT_QUOTES, 'utf-8');
+    }
+    static function arrayToXml($array,&$xml){
+        foreach ($array as $key => $value) {
+            if(is_array($value) || is_object($value)){
+                pars::arrayToXml($value, $xml->addChild($key));
+            }else{
+                $xml->addChild($key, htmlspecialchars($value));
+            }
+        }        
+    }
+    
+    static function JSONtoStr($json,&$result){
+        foreach ($json as $key => $value) {
+            if(is_array($value) || is_object($value)){
+                $result.='Æ';
+                pars::JSONtoStr($value, $result);
+                $result=$result.'Æ';
+            }else{
+                $result.=$key.':'.$value.'Ћ';
+            }
+        }
+    }
+
+}
